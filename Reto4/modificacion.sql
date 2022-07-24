cbc77be829ae057eb01c82143c9cc26d29fb5d98
Carlos-Lopez <carlosedm97@gmail.com>
2022-07-23 22:05:07 -0500

    First commit.

---
+++ Reto4/modificacion.sql
@@ -0,0 +1,3 @@
+UPDATE bicicletas SET año_construccion = 2017 WHERE nombre_fabricante = 'Cannondale';
+UPDATE clientes SET celular = '3115678432' WHERE username = 'ninja';
+DELETE FROM intencionesDeCompra WHERE username = 'green' && nombre_fabricante = 'Trek';
